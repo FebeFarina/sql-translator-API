30ab4e425e2cc3d9afe1601dd47af2a4420571b2
Febe Farina <alu0101430973@ull.edu.es>
2024-04-25 21:36:45 +0100

    Trying vector store

--- src/examples.js
+++ src/examples.js
@@ -1,44 +1,58 @@
 export const examples = [
-  {
-    input: "Tell me the name of 5 actors who have acted in the most movies, along with the number of movies they have acted in.",
-    output: {
-      sqlQuery: "```sql\nSELECT p.person_name, COUNT(mc.person_id) AS movie_count\nFROM movies.person p\nJOIN movies.movie_cast mc ON p.person_id = mc.person_id\nGROUP BY p.person_name\nORDER BY movie_count DESC\nLIMIT 5;\n```",
-      answer: "1. Samuel L. Jackson - 67 movies\n2. Robert De Niro - 57 movies\n3. Bruce Willis - 51 movies\n4. Matt Damon - 48 movies\n5. Morgan Freeman - 46 movies"
+    {
+        "input": "Tell me the name of 5 actors who have acted in the most movies, along with the number of movies they have acted in.",
+        "output": {
+            "sqlQuery": "```sql\nSELECT p.person_name, COUNT(mc.person_id) AS movie_count\nFROM movies.person p\nJOIN movies.movie_cast mc ON p.person_id = mc.person_id\nGROUP BY p.person_name\nORDER BY movie_count DESC\nLIMIT 5;\n```",
+            "answer": "1. Samuel L. Jackson - 67 movies\n2. Robert De Niro - 57 movies\n3. Bruce Willis - 51 movies\n4. Matt Damon - 48 movies\n5. Morgan Freeman - 46 movies"
+        }
+    },
+    {
+        "input": "Tell me about the movies table",
+        "output": {
+            "sqlQuery": "N/A",
+            "answer": "The \"movies\" table contains information about movies, including columns like movie_id, title, budget, homepage, overview, popularity, release_date, revenue, runtime, movie_status, tagline, vote_average, and vote_count. Some sample rows from the table are:\n- Movie ID: 5, Title: Four Rooms, Budget: 4000000, Homepage: Not specified, Overview: It's Ted the Bellhop's first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments., Popularity: 22.876230, Release Date: Sat Dec 09 1995, Revenue: 4300000, Runtime: 98, Movie Status: Released, Tagline: Twelve outrageous guests. Four scandalous requests. And one lone bellhop, in his first day on the job, who's in for the wildest New year's Eve of his life., Vote Average: 6.50, Vote Count: 530\n- Movie ID: 11, Title: Star Wars, Budget: 11000000, Homepage: http://www.starwars.com/films/star-wars-episode-iv-a-new-hope, Overview: Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire., Popularity: 126.393695, Release Date: Wed May 25 1977, Revenue: 775398007, Runtime: 121, Movie Status: Released, Tagline: A long time ago in a galaxy far, far away..., Vote Average: 8.10, Vote Count: 6624\n- Movie ID: 12, Title: Finding Nemo, Budget: 94000000, Homepage: http://movies.disney.com/finding-nemo, Overview: Nemo, an adventurous young clownfish, is unexpectedly taken from his Great Barrier Reef home to a dentist's office aquarium. It's up to his worrisome father Marlin and a friendly but forgetful fish Dory to bring Nemo home -- meeting vegetarian sharks, surfer dude turtles, hypnotic jellyfish, hungry seagulls, and more along the way., Popularity: 85.688789, Release Date: Fri May 30 2003, Revenue: 940335536, Runtime: 100, Movie Status: Released, Tagline: There are 3.7 trillion fish in the ocean, they're looking for one., Vote Average: 7.60, Vote Count: 6122"
+        }
+    },
+    {
+        "input": "List 15 horror movies ordered by its budget, showing title and budget",
+        "output": {
+            "sqlQuery": "```sql\nSELECT title, budget \nFROM movies.movie \nWHERE title LIKE '%Horror%' \nORDER BY budget DESC \nLIMIT 15;\n```",
+            "answer": "1. Movie: Little Shop of Horrors, Budget: $25,000,000\n2. Movie: The Amityville Horror, Budget: $19,000,000\n3. Movie: #Horror, Budget: $1,500,000\n4. Movie: The Horror Network Vol. 1, Budget: $0"
+        }
+    },
+    {
+        "input": "How many characters has Harrison Ford interpreted?",
+        "output": {
+            "sqlQuery": "```sql\nSELECT COUNT(mc.person_id)\nFROM movies.person p\nJOIN movies.movie_cast mc ON p.person_id = mc.person_id\nWHERE p.person_name = 'Harrison Ford';\n```",
+            "answer": "Harrison Ford has interpreted 32 characters."
+        }
+    },
+    {
+        "input": "What is the average revenue of all movies?",
+        "output": {
+            "sqlQuery": "```sql\nSELECT AVG(revenue) AS average_revenue FROM movie;\n```",
+            "answer": "The average revenue of all movies is $41,371,290."
+        }
+    },
+    {
+        "input": "What is the most common keyword?",
+        "output": {
+            "sqlQuery": "```sql\nSELECT kw.keyword_name, COUNT(*) AS keyword_count\nFROM movies.movie_keywords mk\nJOIN movies.keyword kw\nON kw.keyword_id = mk.keyword_id\nGROUP BY kw.keyword_name\nORDER BY keyword_count DESC\nLIMIT 1```",
+            "answer": "The most common keyword is \"woman director\"."
+        }
+    },
+    {
+        "input": "¿Cuál es el nombre largo de la línea 1?",
+        "output": {
+            "sqlQuery": "SELECT linea_nombre_largo FROM titsa.lineas WHERE id_linea = 1 LIMIT 1.",
+            "answer": "TRANVIA L1"
+        }
+    },
+    {
+        "input": "¿Cuál es el nombre de la parada 1001?",
+        "output": {
+            "sqlQuery": "SELECT parada_nombre FROM titsa.paradas WHERE id_parada = 1001 LIMIT 1.",
+            "answer": "COCHERA PARQUE LA REINA"
+        }
     }
-  },
-  {
-    input: "Tell me about the movies table",
-    output: {
-      sqlQuery: "N/A",
-      answer: "The \"movies\" table contains information about movies, including columns like movie_id, title, budget, homepage, overview, popularity, release_date, revenue, runtime, movie_status, tagline, vote_average, and vote_count. Some sample rows from the table are:\n- Movie ID: 5, Title: Four Rooms, Budget: 4000000, Homepage: Not specified, Overview: It's Ted the Bellhop's first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments., Popularity: 22.876230, Release Date: Sat Dec 09 1995, Revenue: 4300000, Runtime: 98, Movie Status: Released, Tagline: Twelve outrageous guests. Four scandalous requests. And one lone bellhop, in his first day on the job, who's in for the wildest New year's Eve of his life., Vote Average: 6.50, Vote Count: 530\n- Movie ID: 11, Title: Star Wars, Budget: 11000000, Homepage: http://www.starwars.com/films/star-wars-episode-iv-a-new-hope, Overview: Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire., Popularity: 126.393695, Release Date: Wed May 25 1977, Revenue: 775398007, Runtime: 121, Movie Status: Released, Tagline: A long time ago in a galaxy far, far away..., Vote Average: 8.10, Vote Count: 6624\n- Movie ID: 12, Title: Finding Nemo, Budget: 94000000, Homepage: http://movies.disney.com/finding-nemo, Overview: Nemo, an adventurous young clownfish, is unexpectedly taken from his Great Barrier Reef home to a dentist's office aquarium. It's up to his worrisome father Marlin and a friendly but forgetful fish Dory to bring Nemo home -- meeting vegetarian sharks, surfer dude turtles, hypnotic jellyfish, hungry seagulls, and more along the way., Popularity: 85.688789, Release Date: Fri May 30 2003, Revenue: 940335536, Runtime: 100, Movie Status: Released, Tagline: There are 3.7 trillion fish in the ocean, they're looking for one., Vote Average: 7.60, Vote Count: 6122"
-    }
-  },
-  {
-    input: "List 15 horror movies ordered by its budget, showing title and budget",
-    output: {
-      sqlQuery: "```sql\nSELECT title, budget \nFROM movies.movie \nWHERE title LIKE '%Horror%' \nORDER BY budget DESC \nLIMIT 15;\n```",
-      answer: "1. Movie: Little Shop of Horrors, Budget: $25,000,000\n2. Movie: The Amityville Horror, Budget: $19,000,000\n3. Movie: #Horror, Budget: $1,500,000\n4. Movie: The Horror Network Vol. 1, Budget: $0"
-    }
-  },
-  {
-    input: "How many characters has Harrison Ford interpreted?",
-    output: {
-      sqlQuery: "```sql\nSELECT COUNT(mc.person_id)\nFROM movies.person p\nJOIN movies.movie_cast mc ON p.person_id = mc.person_id\nWHERE p.person_name = 'Harrison Ford';\n```",
-      answer: "Harrison Ford has interpreted 32 characters."
-    }
-  },
-  {
-    input: "What is the average revenue of all movies?",
-    output: {
-      sqlQuery: "```sql\nSELECT AVG(revenue) AS average_revenue FROM movie;\n```",
-      answer: "The average revenue of all movies is $41,371,290."
-    }
-  },
-  {
-    input: "What is the most common keyword?",
-    output: {
-      sqlQuery: "```sql\nSELECT kw.keyword_name, COUNT(*) AS keyword_count\nFROM movies.movie_keywords mk\nJOIN movies.keyword kw\nON kw.keyword_id = mk.keyword_id\nGROUP BY kw.keyword_name\nORDER BY keyword_count DESC\nLIMIT 1```",
-      answer: "The most common keyword is \"woman director\"."
-    }
-  }
-];
+]
--- src/server.js
+++ src/server.js
@@ -1,16 +1,39 @@
-import express from "express";
-import cors from "cors";
-import { DataSource } from "typeorm";
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import { SqlToolkit } from "langchain/agents/toolkits/sql";
+import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
+import { SemanticSimilarityExampleSelector } from "@langchain/core/example_selectors";
 import {
+  FewShotPromptTemplate,
+  PromptTemplate,
   ChatPromptTemplate,
-  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
   MessagesPlaceholder,
 } from "@langchain/core/prompts";
-import { ChatOpenAI } from "@langchain/openai";
-import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
-import { SqlToolkit } from "langchain/agents/toolkits/sql";
-import { AIMessage } from "@langchain/core/messages";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { createRetrieverTool } from "langchain/tools/retriever";
 import { SqlDatabase } from "langchain/sql_db";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
+import cors from "cors";
+import express from "express";
+import fs from "fs";
+import { DataSource } from "typeorm";
+
+import { examples } from "./examples.js";
+
+async function queryAsList(database, query) {
+  const res = JSON.parse(
+    await database.run(query)
+  )
+    .flat()
+    .filter((el) => el != null);
+  const justValues = res.map((item) =>
+    Object.values(item)[0]
+      .replace(/\b\d+\b/g, "")
+      .trim()
+  );
+  return justValues;
+}
 
 const connect = async (params) => {
   const datasource = new DataSource({
@@ -45,6 +68,7 @@ app.use(
 
 app.post("/", async (req, res) => {
   try {
+
     console.log("Creating database connection...");
 
     const datasource = new DataSource({
@@ -63,11 +87,53 @@ app.post("/", async (req, res) => {
       appDataSource: datasource,
     });
 
-    console.log(db.allTables.map((t) => t.tableName));
+    const tables = db.allTables;
+    const schema = req.body.databaseInfo.schema;
+    const tablesWithSchema = tables.map(table => `${schema}.${table.tableName}`);
 
-    const llm = new ChatOpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
+
+    console.log("Tables in database: ", tablesWithSchema.join(", "));
+
+    let properNouns = await queryAsList(db, "SELECT parada_nombre FROM titsa.paradas");
+    properNouns = properNouns.concat(await queryAsList(db, "SELECT linea_nombre_largo FROM titsa.lineas"));
+
+    console.log(properNouns.length + " proper nouns found")
+    console.log(properNouns.slice(0, 10) + "...")
+
+    const vectorDb = await MemoryVectorStore.fromTexts(
+      properNouns,
+      {},
+      new OpenAIEmbeddings()
+    );
+
+    console.log("Creating tools...")
+
+    const description = `Use to look up values to filter on.
+    Input is an approximate spelling of the proper noun, output is valid proper nouns.
+    Use the noun most similar to the search.`;
+    const retrieverTool = createRetrieverTool(retriever, {
+      description,
+      name: "search_proper_nouns",
+    });
+
+    const retriever = vectorDb.asRetriever(15);
+
+    console.log("Tools created")
+
+    const llm = new ChatOpenAI({ model: "gpt-4", temperature: 0 });
     const sqlToolKit = new SqlToolkit(db, llm);
-    const tools = sqlToolKit.getTools();
+    const tools = tools = [...sqlToolKit.getTools(), retrieverTool];
+
+    const exampleSelector = await SemanticSimilarityExampleSelector.fromExamples(
+      examples,
+      new OpenAIEmbeddings(),
+      HNSWLib,
+      {
+        k: 5,
+        inputKeys: ["input"],
+      }
+    );
+
     const SQL_PREFIX = `You are an agent designed to interact with a SQL database.
     Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
     Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results using the LIMIT clause.
@@ -80,23 +146,37 @@ app.post("/", async (req, res) => {
     
     DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
     
+    You have access to the following tables: {table_names}
+    
     If the question does not seem related to the database, just return "I don't know" as the answer.
     The output format should be: "SQL Query: <query> Answer: <answer>. If no SQL query is needed, just return the answer.
     When writting the SQL query, use the table names and column names as they are in the database. Dont forget about the schema name if it is needed.`;
+
     const SQL_SUFFIX = `Begin!
-    
-    Question: {input}
-    Thought: I should look at the tables in the database to see what I can query.
-    {agent_scratchpad}`;
-    const prompt = ChatPromptTemplate.fromMessages([
-      ["system", SQL_PREFIX],
-      HumanMessagePromptTemplate.fromTemplate("{input}"),
-      new AIMessage(SQL_SUFFIX.replace("{agent_scratchpad}", "")),
+      Question: {input}
+      Thought: I should look at the tables and columns in the database to see what I can query.
+      {agent_scratchpad}`;
+
+    const fewShotPrompt = new FewShotPromptTemplate({
+      exampleSelector,
+      examplePrompt: PromptTemplate.fromTemplate(
+        "User input: {input} output: {output}"
+      ),
+      inputVariables: ["input", "dialect", "top_k", "table_names"],
+      prefix: SQL_PREFIX,
+      suffix: "",
+    });
+
+    const fullPrompt = ChatPromptTemplate.fromMessages([
+      new SystemMessagePromptTemplate(fewShotPrompt),
+      ["human", "{input}"],
       new MessagesPlaceholder("agent_scratchpad"),
     ]);
-    const newPrompt = await prompt.partial({
+
+    const newPrompt = await fullPrompt.partial({
       dialect: sqlToolKit.dialect,
       top_k: "10",
+      table_names: tablesWithSchema.join(", "),
     });
     const runnableAgent = await createOpenAIToolsAgent({
       llm,
@@ -130,6 +210,29 @@ app.post("/", async (req, res) => {
         : null;
     const answer =
       answerIndex < outputParts.length ? outputParts[answerIndex].trim() : null;
+
+    examples.push({
+      input: req.body.query,
+      output: {
+        sqlQuery,
+        answer,
+      },
+    });
+
+    console.log("Save result? (Y to save)")
+
+    process.stdin.on("data", (data) => {
+      const input = data.toString().trim();
+      if ((input === "Y" || input === "y") && answer != "I don't know.") {
+        fs.writeFileSync("./src/examples.js", `export const examples = ${JSON.stringify(examples, null, 2)
+          }`);
+        console.log("Result saved");
+      } else {
+        console.log("Result not saved");
+      }
+    });
+
+    console.log("Sending response...")
     res.status(200).send({ sqlQuery, answer });
 
   } catch (e) {
